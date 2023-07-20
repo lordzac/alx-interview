6bf991c5843874a0f23a0beb4954736cb5439422
lordzac <balogunzacchaeus@gmail.com>
2023-07-21 00:00:49 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,41 @@
 #!/usr/bin/python3
-"""
-Task - Script that reads stdin line by line and computes metrics
-"""
-
+""" reads stdin line by line and computes metrics """
 import sys
 
 
 if __name__ == "__main__":
-    st_code = {"200": 0,
-               "301": 0,
-               "400": 0,
-               "401": 0,
-               "403": 0,
-               "404": 0,
-               "405": 0,
-               "500": 0}
+    status = {"200": 0,
+              "301": 0,
+              "400": 0,
+              "401": 0,
+              "403": 0,
+              "404": 0,
+              "405": 0,
+              "500": 0}
     count = 1
     file_size = 0
 
-    def parse_line(line):
-        """ Read, parse and grab data"""
+    def get_line(line):
+        """ parse and grab data"""
         try:
             parsed_line = line.split()
             status_code = parsed_line[-2]
-            if status_code in st_code.keys():
-                st_code[status_code] += 1
+            if status_code in status.keys():
+                status[status_code] += 1
             return int(parsed_line[-1])
         except Exception:
             return 0
 
     def print_stats():
-        """print stats in ascending order"""
+        """print stats"""
         print("File size: {}".format(file_size))
-        for key in sorted(st_code.keys()):
-            if st_code[key]:
-                print("{}: {}".format(key, st_code[key]))
+        for key in sorted(status.keys()):
+            if status[key]:
+                print("{}: {}".format(key, status[key]))
 
     try:
         for line in sys.stdin:
-            file_size += parse_line(line)
+            file_size += get_line(line)
             if count % 10 == 0:
                 print_stats()
             count += 1
